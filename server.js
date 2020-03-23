d390bf9046cebbd2eb490c93b26bf9edad2eaf23
Anna Flávia <anna.castro@truckpad.com.br>
2020-03-23 03:13:16 -0300

    Adding project to the repository

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const server = express();
+
+server.use(express.static("public"));
+
+server.use(express.urlencoded({ extended: true }));
+
+const Pool = require("pg").Pool;
+const db = new Pool({
+    user: "anna.castro",
+    password: "12345",
+    host: "localhost",
+    port: 5432,
+    database: "doe",
+});
+
+const nunjucks = require("nunjucks");
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+});
+
+server.get("/", function(req, res) {
+    db.query("SELECT * FROM donors ORDER BY created_at DESC LIMIT 4", function(error, result) {
+        if (error) return res.send("Erro no banco de dados");
+    
+    const donors = result.rows;
+
+    return res.render("index.html", { donors });
+    });
+});
+
+server.post("/", function(req, res) {
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios")
+    };
+
+    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1, $2, $3)`
+    const values = [name, email, blood];
+
+    db.query(query, values, function(error) {
+        if (error) return res.send("Erro no banco de dados");
+    return res.redirect("/")
+    });
+});
+
+server.listen(3000);
